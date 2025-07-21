df3855b2a07e7e0a53e70dd1334816d49cd05f80
emintz <emintz@google.com>
2025-07-21 16:39:09 -0400

    Add license and perform minor cleanup.

--- src/TraversalStatus.h
+++ src/TraversalStatus.h
@@ -4,6 +4,22 @@
  *
  * Traversal status that determines behavior. See
  * enumerations for details.
+ *
+ * Copyright (C) 2025 Eric Mintz
+ * All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef TRAVERSAL_STATUS_H_
@@ -11,22 +27,28 @@
 
 namespace VisitingParseTree {
 
+/*
+ * Indicates the state of a traversal and the
+ * next action that should be taken.
+ */
 enum class TraversalStatus {
   /*
    * Continue running the traversal. Process children
-   * after entry; return after exit.
+   * after entry; return after exit. Traversal is OK.
    */
   CONTINUE,
   /*
    * Bypass processing children on exit; equivalent to
-   * CONtiNUE on exit.
+   * CONtiNUE on exit. Traversal is OK.
    */
   BYPASS_CHILDREN,
   /*
    * Cancel the traversal immediately. The traversal stops
    * WITHOUT invoking additional actions. In particular,
    * if if returned on entry, the bypass both the node's
-   * children AND exit processing.
+   * children AND exit processing. MIGHT indicate an
+   * error or might simply mean that further processing
+   * is not required.
    */
   CANCEL,
 };
--- src/VacuousVoidFunction.cpp
+++ src/VacuousVoidFunction.cpp
@@ -3,6 +3,22 @@
  *
  *  Created on: Jul 20, 2025
  *      Author: Eric Mintz
+ *
+ * Copyright (C) 2025 Eric Mintz
+ * All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "VacuousVoidFunction.h"
@@ -10,6 +26,9 @@
 namespace VisitingParseTree {
 
 VacuousVoidFunction VacuousVoidFunction::INSTANCE;
+
+VacuousVoidFunction::~VacuousVoidFunction() {
+}
 
 void VacuousVoidFunction::operator ()(void) {
   // Do nothing.
